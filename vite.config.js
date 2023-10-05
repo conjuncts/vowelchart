09782b2a12b28b500302929e7a2f04305a6d70d8
conjuncts <67614673+conjuncts@users.noreply.github.com>
2023-10-05 17:52:01 -0500

    try to externalize d3 again

--- vite.config.js
+++ vite.config.js
@@ -4,14 +4,7 @@ export default defineConfig({
     base: '/vowelchart/',
     build: {
         rollupOptions: {
-            external: ['d3'],
-            output: {
-                // Provide global variables to use in the UMD build
-                // for externalized deps
-                globals: {
-                    d3: 'd3',
-                },
-            },
+            external: ['d3']
         }
     }
 });
