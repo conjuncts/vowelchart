971b4798bcb4e9cbbc0bc9d708fdc77b50ba122f
conjuncts <67614673+conjuncts@users.noreply.github.com>
2023-10-05 17:56:29 -0500

    try again at deploy

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,8 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/vowelchart/',
     build: {
-        rollupOptions: {
-            external: ['d3']
-        }
+        // rollupOptions: {
+        //     external: ['d3']
+        // }
     }
 });
